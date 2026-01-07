16fdb6ae797facf82cfa2ed9a444b47e67828a0b
Pratham <pratham10600@gmail.com>
2026-01-07 21:06:34 +0530

    sir soln

--- c10_Arrays/medium/sort_inplace.cpp
+++ c10_Arrays/medium/sort_inplace.cpp
@@ -16,6 +16,8 @@ Output: [0]
 #include  <bits/stdc++.h>
 using namespace std;
 
+
+// mine
 vector<int> insort(vector <int> arr,int n){
     for (int i=0;i<n;i++){
         for(int j=0;j<n;j++){
@@ -26,6 +28,36 @@ vector<int> insort(vector <int> arr,int n){
     }
     return arr;
 }
+
+// sir
+
+// brute force ->  merge sort tc: nlogn, sc: n
+
+// better: tc: O(2n) like counting sort
+
+vector<int> insort2(vector<int> arr, int n){
+    int cnt0=0,cnt1=0,cnt2=0;
+    for(int i=0;i<n;i++){
+        if (arr[i]==0){
+            cnt0++;
+        }
+        else if (arr[i]==1){
+            cnt1++;
+        }
+        else if (arr[i]==2){
+            cnt2++;
+        }
+        else{continue;}
+    }
+
+    for(int j=0;j<cnt0;j++) arr[j]=0;
+    for(int j=cnt0;j<cnt0+cnt1;j++) arr[j]=1;
+    for(int j=cnt0+cnt1;j<n;j++) arr[j]=2;
+
+    return arr;
+}
+
+// optimal tc: O(n) -> dutch national flag algo (DNFA)
 int main(){
     int n;
     cout<<"enter no of elements";
@@ -37,9 +69,9 @@ int main(){
         arr.push_back(temp);
     }
     cout<<"after sort"<<endl;
-    vector<int> result=insort(arr,n);
+    vector<int> result=insort2(arr,n);
     for(auto it:result){
-        cout<<it<<endl;
+        cout<<it<<" ";
     }
 
 
