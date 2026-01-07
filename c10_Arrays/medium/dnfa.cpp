20dc3d307edce740a5f1d5109c6e1cb8d6c2e76e
Pratham <pratham10600@gmail.com>
2026-01-07 21:52:38 +0530

    dnfa

--- c10_Arrays/medium/dnfa.cpp
+++ c10_Arrays/medium/dnfa.cpp
@@ -1,1 +1,47 @@
 // dutch national flag algo
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> dnfa(vector <int>arr,int n){
+    int low=0,mid=0,high=n-1;
+
+    while (mid<=high)
+    {
+        if (arr[mid==0]){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+    
+    return arr;
+}
+
+int main(){
+    vector<int> arr;
+    int n;
+    cout<<"Enter no of numbers";
+    cin>>n;
+    int temp;
+        
+
+    for(int i=0;i<n;i++){
+        cin>> temp;
+        arr.push_back(temp);
+    }
+
+
+    vector <int> result=dnfa(arr,n);
+    for(auto it:result){
+        cout<<it<<" ";
+    }
+}
