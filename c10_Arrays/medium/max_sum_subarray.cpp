5ae40fc6e905d267601cbb915c9ee333f0758324
Pratham <pratham10600@gmail.com>
2026-01-11 17:31:52 +0530

    max_sum

---
+++ c10_Arrays/medium/max_sum_subarray.cpp
@@ -0,0 +1,37 @@
+// Given an integer array nums, find the subarray with the largest sum and return the sum of the elements present in that subarray.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// mine
+int max_sum(vector<int> arr,int n){
+    int max_sum_arr=INT_MIN;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int sum=0;
+            for (int k=i;k<=j;k++){
+                 sum+=arr[k];
+            }
+            if (sum>max_sum_arr){
+                max_sum_arr=sum;
+            }
+        }
+    }
+    return max_sum_arr;
+}
+
+int main(){
+    cout<<"enter no of elements:";
+    int n;
+    cin>>n;
+
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int temp;
+        cin>>temp;
+        arr.push_back(temp);
+    }
+
+    cout<<"Max sum of subarray is:"<<max_sum(arr,n);
+
+}
