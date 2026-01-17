f594839fdc67481413c0596669e7c7c783470ef1
Pratham <pratham10600@gmail.com>
2026-01-17 22:11:28 +0530

    bruteforce

--- c10_Arrays/medium/alternate_pos_neg.cpp
+++ c10_Arrays/medium/alternate_pos_neg.cpp
@@ -1,6 +1,31 @@
 #include<bits./stdc++.h>
 using namespace std;
 
+// sir
+
+// brute force
+vector<int> alt2(vector<int>arr,int n){
+    vector<int> pos_arr;
+    vector<int> neg_arr;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            pos_arr.push_back(arr[i]);
+        }else{
+            neg_arr.push_back(arr[i]);
+        }
+    }
+    arr.clear();
+    for (int j=0;j<n/2;j++){
+        arr.push_back(pos_arr[j]);
+        arr.push_back(neg_arr[j]);
+        
+    }
+
+    return arr;
+}
+
+
 // mine two pointer approach -> under progress tc: O(n) sc:O(n)
 vector<int> alt(vector<int>arr,int n){
     int pos_postion=0,neg_postion=0;
@@ -25,6 +50,10 @@ int main(){
         arr.push_back(temp);
     }
 
-    cout<<"Max sum of subarray is:";
-
+    cout<<"result array is:"<<endl;
+    vector<int> result=alt2(arr,n);
+    for(auto it:result){
+        cout<<it<<" ";
+    }
+    
 }
