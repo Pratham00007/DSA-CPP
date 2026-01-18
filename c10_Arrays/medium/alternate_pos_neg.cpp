35675697997872aa9d781d3f86db0a395851dfd1
Pratham <pratham10600@gmail.com>
2026-01-18 18:57:28 +0530

    optimized

--- c10_Arrays/medium/alternate_pos_neg.cpp
+++ c10_Arrays/medium/alternate_pos_neg.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // sir
 
-// brute force
+// brute force sc=O(2N) sc=O(N)
 vector<int> alt2(vector<int>arr,int n){
     vector<int> pos_arr;
     vector<int> neg_arr;
@@ -15,14 +15,30 @@ vector<int> alt2(vector<int>arr,int n){
             neg_arr.push_back(arr[i]);
         }
     }
-    arr.clear();
     for (int j=0;j<n/2;j++){
-        arr.push_back(pos_arr[j]);
-        arr.push_back(neg_arr[j]);
+        arr[2*j]=pos_arr[j];
+        arr[2*j+1]=neg_arr[j];
         
     }
 
     return arr;
+}
+
+// optimal:same as mine. sc=O(N) tc=O(n)
+vector<int> alt3(vector<int>arr,int n){
+    int pos=0,neg=1;
+    vector<int> new_Arr(n,0);
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            new_Arr[pos]=arr[i];
+            pos+=2;
+        }else{
+            new_Arr[neg]=arr[i];
+            neg+=2;
+        }
+
+    }
+    return new_Arr;
 }
 
 
@@ -38,8 +54,46 @@ vector<int> alt(vector<int>arr,int n){
     }
 }
 
+// type-2 if any left then add them in last use brute force one bcs optimal cant
+vector<int> alt4(vector<int>arr,int n){
+    vector<int> pos_arr;
+    vector<int> neg_arr;
+
+    for(int i=0;i<n;i++){
+        if(arr[i]>0){
+            pos_arr.push_back(arr[i]);
+        }else{
+            neg_arr.push_back(arr[i]);
+        }
+    }
+    if(pos_arr.size()>neg_arr.size()){
+        for(int i=0;i<neg_arr.size();i++){
+            arr[2*i]=pos_arr[i];
+            arr[2*i+1]=neg_arr[i];
+        }
+        int index=neg_arr.size()*2;
+        for(int i=pos_arr.size();i<neg_arr.size();i++){
+            arr[index]=pos_arr[i];
+            index++;
+        }
+    }else{
+        for(int i=0;i<pos_arr.size();i++){
+            arr[2*i]=pos_arr[i];
+            arr[2*i+1]=neg_arr[i];
+        }
+        int index=pos_arr.size()*2;
+        for(int i=pos_arr.size();i<neg_arr.size();i++){
+            arr[index]=neg_arr[i];
+            index++;
+        }
+
+    }
+
+    return arr;
+}
+
 int main(){
-    cout<<"enter no of elements:";
+    cout<<"enter no of elements:"; 
     int n;
     cin>>n;
 
@@ -51,7 +105,7 @@ int main(){
     }
 
     cout<<"result array is:"<<endl;
-    vector<int> result=alt2(arr,n);
+    vector<int> result=alt4(arr,n);
     for(auto it:result){
         cout<<it<<" ";
     }
