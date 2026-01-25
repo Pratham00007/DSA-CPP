fd3f860881604d99f4a3408cac12d2e54dc658b2
Pratham <pratham10600@gmail.com>
2026-01-25 12:15:54 +0530

    completed

--- c10_Arrays/medium/explaination.md
+++ c10_Arrays/medium/explaination.md
@@ -21,3 +21,6 @@ mosse's voting algo
 keedans algo
 
 ![alt text](image-8.png)
+
+
+![alt text](image-9.png)
--- c10_Arrays/medium/set_matrix_zero.cpp
+++ c10_Arrays/medium/set_matrix_zero.cpp
@@ -15,6 +15,7 @@ Explanation:Since matrix[0][0]=0 and matrix[0][3]=0. Therefore 1st row, 1st colu
 #include <bits/stdc++.h>
 using namespace std;
 
+// brute force O(n3)
 void zero1(vector<vector<int>>&arr){
     int m=arr.size(); // no of rows
     int n=arr[0].size(); // no of columns
@@ -53,11 +54,84 @@ void zero1(vector<vector<int>>&arr){
 }
 
 
+// beter tc(2*m*n) sc(m+n)
+
+void zero2(vector<vector<int>>&arr){
+    int m=arr.size();
+    int n=arr[0].size();
+
+    int row[m]={0};
+    int col[n]={0};
+    for (int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(arr[i][j]==0){
+                row[i]=1;
+                col[j]=1;
+            }
+        }
+    }
+
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(row[i]==1 || col[j]==1)
+            arr[i][j]=0;
+        }
+    }
+}
+
+// optimal Sc=O(1) tc(P\O(n2))
+void zero3(vector<vector<int>>&arr){
+// int  col[m]= {0}; martrix[0][..]
+// int row[0]={0}; martrix[..][0];
+int n=arr.size(); // no of rows
+int m=arr[0].size(); // no of columns
+int col0=1;
+for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+        if(arr[i][j]==0){
+            // mark ith row 
+            arr[i][0]=0;
+            
+            // mark jth col
+            if(j!=0){
+                arr[0][j]=0;
+
+            }
+            else{
+                col0=0;
+            }
+        }
+    }
+}
+for (int i=1;i<n;i++){
+    for(int j=1;j<m;j++){
+        if(arr[i][j]!=0){
+            // check for col and row
+            if(arr[0][j]==0||arr[i][0]==0){
+                arr[i][j]=0;
+            }
+        }
+    }
+}
+
+if(arr[0][0]==0){
+    for(int j=0;j<m;j++){
+        arr[0][j]=0;
+
+    }
+}
+if (col0==0){
+    for(int i=0;i<n;i++){
+        arr[i][0]=0;
+    }
+}
+}
+
 int main(){
      // Example matrix
     vector<vector<int>> matrix = {{1,1,1},{1,0,1},{1,1,1}};
     
-    zero1(matrix);
+    zero3(matrix);
     
     // Print final matrix
     for (auto row : matrix) {
