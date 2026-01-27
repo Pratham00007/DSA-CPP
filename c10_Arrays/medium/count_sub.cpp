c04b3368dabf730a10d04c03bf7c16e6e5669015
Pratham <pratham10600@gmail.com>
2026-01-27 22:43:05 +0530

    done_by_me

---
+++ c10_Arrays/medium/count_sub.cpp
@@ -0,0 +1,40 @@
+/*
+Count Subarray sum Equals K
+
+Problem Statement: Given an array of integers and an integer k, return the total number of subarrays whose sum equals k. A subarray is a contiguous non-empty sequence of elements within an array.
+
+Examples
+Input : N = 4, array[] = {3, 1, 2, 4}, k = 6
+Output: 2
+Explanation: The subarrays that sum up to 6 are [3, 1, 2] and [2, 4].
+
+Input: N = 3, array[] = {1,2,3}, k = 3
+Output: 2
+Explanation: The subarrays that sum up to 3 are [1, 2], and [3].
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// mine
+int subarr(int n,int arr[],int k){
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        int sum=0;
+        for(int j=i;j<n;j++){
+            sum+=arr[j];
+            if(sum==k){
+                cnt++;
+            }
+
+        }
+        
+    }
+    return cnt;
+}
+
+int main(){
+    int arr[3]={1,2,3};
+    cout<<subarr(3,arr,3);
+}
