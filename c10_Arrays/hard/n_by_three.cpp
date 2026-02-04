1ac1bbe4f7a710c1e80a9a6b44798ab3454878ef
Pratham <pratham10600@gmail.com>
2026-02-04 22:19:08 +0530

    optimized

--- c10_Arrays/hard/n_by_three.cpp
+++ c10_Arrays/hard/n_by_three.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 
 
-// sir brute force
+// sir brute force hashing
 vector<int> answ2(vector<int> arr){
     vector<int>ls;
     int n=arr.size();
@@ -17,6 +17,46 @@ vector<int> answ2(vector<int> arr){
     return ls;
 }
 
+// sir optimal moones voting algo approach
+
+vector<int> answ(vector<int> arr){
+    int ele1=INT_MIN,ele2=INT_MIN;
+    int cnt1=0,cnt2=0;
+    for(int i=0;i<arr.size();i++){
+        if(cnt1==0 && ele2!=arr[i]){
+            cnt1=1;
+            ele1=arr[i];
+        }
+        else if(cnt2==0 && ele1!=arr[i]){
+            cnt2=1;
+            ele2=arr[i];
+        }
+        else if(arr[i]==ele1){cnt1++;}
+        else if(arr[i]==ele2){cnt2++;}
+        else{
+            cnt1--;cnt2--;
+        }
+        
+    }
+    // validating
+    cnt1=0;cnt2=0;
+    int n=arr.size();
+    for(int i=0;i<n;i++){
+        if(arr[i]==ele1){cnt1++;}
+        else if(arr[i]==ele2){cnt2++;}
+
+    }
+    int mini=n/3+1;
+    vector<int>res;
+    if (cnt1>=mini){res.push_back(ele1);}
+    if(cnt2>=mini){
+        res.push_back(ele2);
+    }
+    return res;
+}
+
+
+// mine
 vector<int> answ(vector<int> arr){
     int n=arr.size();
     vector<int>ans;
