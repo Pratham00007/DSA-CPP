0506d175a8914a5b553840e7a1ac441b12bc7374
Pratham <pratham10600@gmail.com>
2026-04-01 20:14:14 +0530

    mine

---
+++ binary_search/5_last_occurence.cpp
@@ -0,0 +1,53 @@
+/*
+Last occurrence in a sorted array
+
+Problem Statement: Given a sorted array of N integers, write a program to find the index of the last occurrence of the target key. If the target is not found then return -1. Note: Consider 0 based indexing
+
+Examples
+Example 1:
+Input:
+ N = 7, target = 13, array[] = {3, 4, 13, 13, 13, 20, 40}  
+Output:
+ 4  
+Explanation:
+ The target value 13 appears for the first time at index number 2 in the array.  
+
+Example 2:
+Input:
+ N = 7, target = 60, array[] = {3, 4, 13, 13, 13, 20, 40}  
+Output:
+ -1  
+Explanation:
+ Target value 60 is not present in the array, so the output is -1.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// mine find ub-1
+int l_index(vector<int>arr, int target)
+{
+    int n=arr.size();
+    int l=0,h=n-1;
+    int ans=-1;
+    while(l<=h){
+        int mid=(l+h)/2;
+        if(arr[mid]>target){
+            ans=mid-1;
+            h=mid-1;
+        }else{            
+            l=mid+1;
+        }
+    }
+    return ans;
+}
+
+// trying in same code for least and highest index
+
+int main(){
+    vector<int>arr={3, 4, 13, 13, 13, 20, 40};
+    cout<<l_index(arr,0);
+
+}
