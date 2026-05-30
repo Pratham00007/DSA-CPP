7d48ad14caebca0c0786cbaedd943308a9c3da47
Pratham <pratham10600@gmail.com>
2026-05-30 11:43:29 +0530

    optimal gas

--- binary_search/bs_on_answers/11_gas_station.cpp
+++ binary_search/bs_on_answers/11_gas_station.cpp
@@ -71,6 +71,45 @@ return pq.top().first;
 
 // optimal with no sc
 
+ int numberOfGasStationsRequired(long double dist, vector<int> &arr) {
+    int n = arr.size();
+    int cnt = 0;
+
+    for (int i = 1; i < n; i++) {
+        // Number of stations needed between arr[i-1] and arr[i]
+        int numberInBetween = (arr[i] - arr[i - 1]) / dist;
+
+        // If perfectly divisible, reduce one to avoid extra placement
+        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
+            numberInBetween--;
+        }
+        cnt += numberInBetween;
+    }
+    return cnt;
+}
+
+    // Function to minimize the maximum distance between gas stations
+long double minimiseMaxDistance(vector<int> &arr, int k) {
+    int n = arr.size();
+    long double low = 0, high = 0;
+
+    // Determine max initial distance between stations
+    for (int i = 0; i < n - 1; i++) {
+        high = max(high, (long double)(arr[i + 1] - arr[i]));
+    }
+
+    long double diff = 1e-6;
+
+    // Binary search to find minimum possible maximum distance
+    while (high - low > diff) {
+        long double mid = (low + high) / 2.0;
+        int cnt = numberOfGasStationsRequired(mid, arr);
+        if (cnt > k) low = mid;
+        else high = mid;
+    }
+
+    return high;
+}
 
 int main(){
      vector<int> arr = {1, 2, 3, 4, 5};
--- binary_search/bs_on_answers/explaination.md
+++ binary_search/bs_on_answers/explaination.md
@@ -98,3 +98,7 @@ stores in deceding ie high to low
 ![alt text](image-32.png)
 
 ![alt text](image-33.png)
+
+optimal bs
+
+![alt text](image-34.png)
