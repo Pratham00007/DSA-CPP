5d1ee2378646adfb960e880628a7becdde63346f
Pratham <pratham10600@gmail.com>
2026-07-09 20:21:56 +0530

    rev

---
+++ revision/rot.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// class Solution {
+// public:
+//     // Check if 'goal' is a rotation of 's'
+//     bool rotateString(string& s, string& goal) {
+//         // Strings must be same length to be rotations of each other
+//         if (s.length() != goal.length()) {
+//             return false;
+//         }
+//         // Try all possible rotations of 's'
+//         for (int i = 0; i < s.length(); i++) {
+//             string rotated = s.substr(i) + s.substr(0, i);  
+//             cout<<rotated<<endl;
+//             if (rotated == goal) {
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// int main() {
+//     Solution sol;
+//     string s = "rotation";
+//     string goal = "tionrota";
+    
+//     // Output the result
+//     if (sol.rotateString(s, goal)) {
+//         cout << "true" << endl;  
+//     } else {
+//         cout << "false" << endl;
+//     }
+
+//     return 0;
+// }
+
+int main(){
+string s="012";
+cout<<s.substr(0);
+}
