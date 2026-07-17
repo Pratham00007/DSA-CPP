5ab132439f832c655629ae89bdb0e99804158fa2
Pratham <pratham10600@gmail.com>
2026-07-17 22:24:54 +0530

    fruits in basket

---
+++ sliding_window/3_fruits.cpp
@@ -0,0 +1,61 @@
+/*
+Fruit Into Baskets
+
+
+7
+
+Problem Statement: There is only one row of fruit trees on the farm, oriented left to right.
+ An integer array called fruits represents the trees, where fruits[i] denotes the kind of 
+ fruit produced by the ith tree.
+The goal is to gather as much fruit as possible, adhering to the owner's stringent rules :
+
+There are two baskets available, and each basket can only contain one kind of fruit. 
+The quantity of fruit each basket can contain is unlimited.
+Start at any tree, but as you proceed to the right, select exactly one fruit from 
+each tree, including the starting tree. One of the baskets must hold the harvested fruits.
+Once reaching a tree with fruit that cannot fit into any basket, stop.
+Return the maximum number of fruits that can be picked.
+
+Examples
+Input :fruits = [1, 2, 1]
+Output :3
+Explanation : We will start from first tree.
+The first tree produces the fruit of kind '1' and we will put that in the first basket.
+The second tree produces the fruit of kind '2' and we will put that in the second basket.
+The third tree produces the fruit of kind '1' and we have first basket that is
+ already holding fruit of kind '1'. So we will put it in first basket.
+Hence we were able to collect total of 3 fruits.
+
+
+Input : fruits = [1, 2, 3, 2, 2]
+Output : 4
+Explanation : we will start from second tree.
+The first basket contains fruits from second , fourth and fifth.
+The second basket will contain fruit from third tree.
+Hence we collected total of 4 fruits.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int fruit(vector<int>arr,int k){
+    int n=arr.size();
+    int l=0,r=0,maxlen=0;
+    map<int,int>mpp;
+    while(r<n){
+        mpp[arr[r]]++;
+        if(mpp.size()>k){
+            mpp[arr[l]]--;
+            if(mpp[arr[l]]==0) mpp.erase(arr[l]);
+            l++;
+        }
+        if(mpp.size()<=k) maxlen=max(maxlen,r-l+1);
+        r++;
+    }
+    return maxlen;
+}
+
+
+int main(){
+
+}
--- sliding_window/explain.md
+++ sliding_window/explain.md
@@ -77,3 +77,62 @@ till zeros cond meet
 ![alt text](image-29.png)
 
 ![alt text](image-30.png)
+
+3_fruits
+
+![alt text](image-31.png)
+2 basket are there
+basket can contain only single type of fruit
+
+pickup must be cosequetive broken pickup not allowed
+
+![alt text](image-32.png)
+
+![alt text](image-33.png)
+
+brute
+
+![alt text](image-34.png)
+cant go beyond 3 aa gaye
+
+![alt text](image-35.png)
+
+![alt text](image-36.png)
+
+optimal sliding window
+![alt text](image-37.png)
+
+after inserting checking valid number in the set to check if that valid or not?
+possible
+![alt text](image-38.png)
+
+![alt text](image-39.png)
+
+![alt text](image-40.png)
+not possible
+
+starty trimming from left
+![alt text](image-41.png)
+still not possible
+
+![alt text](image-42.png)
+
+![alt text](image-43.png)
+
+![alt text](image-44.png)
+
+![alt text](image-45.png)
+
+optimal O(n)
+![alt text](image-46.png)
+
+![alt text](image-47.png)
+![alt text](image-48.png)
+
+![alt text](image-49.png)
+
+![alt text](image-50.png)
+![alt text](image-51.png)
+
+![alt text](image-53.png)
+![alt text](image-52.png)
