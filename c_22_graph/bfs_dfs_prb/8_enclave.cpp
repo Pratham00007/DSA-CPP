df1e8268821c5fcbd0e4c213f0e0ffaa4e91ffa8
Pratham <pratham10600@gmail.com>
2026-09-15 23:49:14 +0530

    8_enclave

---
+++ c_22_graph/bfs_dfs_prb/8_enclave.cpp
@@ -0,0 +1,65 @@
+/*
+
+Number of Enclaves
+
+
+2
+
+Problem Statement: You are given an N x M binary matrix grid, 
+where 0 represents a sea cell and 1 represents a land cell. 
+A move consists of walking from one land cell to another adjacent 
+(4-directionally) land cell or walking off the boundary of the grid.
+ Find the number of land cells in the grid for which we cannot walk
+  off the boundary of the grid in any number of moves..
+
+  
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int noEnclave(vector<vector<int>>&grid){
+    queue<pair<int,int>>q;
+    int n=grid.size();
+    int m=grid[0].size();
+    int vis[n][m]={0};
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(i==0|| j==0|| i==n-1|| j==m-1){
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                }
+            }
+        }
+    }
+
+    int delrow[]={-1,0,+1,0};
+    int delcol[]={0,+1,+0,-1};
+
+    while(!q.empty()){
+        int row=q.front().first;
+        int col=q.front().second;
+        q.pop();
+
+        for(int i=0;i<4;i++){
+            int nrow=row+delrow[i];
+            int ncol=col+delcol[i];
+            if(nrow>=0 && nrow<n && ncol>=0  && ncol<m 
+            && vis[nrow][ncol]==0&& grid[nrow][ncol]==1){
+                q.push({nrow,ncol});
+                vis[nrow][ncol]=1;
+            }
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(grid[i][j]==1 && vis[i][j]==0) cnt++;
+        }
+    }
+
+    return cnt;
+
+}
+
--- c_22_graph/bfs_dfs_prb/explain.md
+++ c_22_graph/bfs_dfs_prb/explain.md
@@ -179,3 +179,17 @@ APPLY BFS
 ![ ](image-70.png)
 
 ![alt text](image-71.png)
+
+
+8_enclave
+
+no of 1 where we cant go outside is answer
+
+![alt text](image-73.png)
+
+![alt text](image-74.png)
+
+![alt text](image-76.png)
+
+![alt text](image-75.png)
+
