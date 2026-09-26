23912d04cf036feddca6f54a9a1b560376c42785
Pratham <pratham10600@gmail.com>
2026-09-26 23:01:03 +0530

    11

---
+++ c_22_graph/hard_prb/11_word1.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int wordLadder(string startWord,string targetword,vector<string>&wordList){
+    queue<pair<string,int>>q;
+    q.push({startWord,1});
+    unordered_set<string>st(wordList.begin(),wordList.end());
+    st.erase(startWord);
+    while(!q.empty()){
+        string word=q.front().first;
+        int steps=q.front().second;
+        q.pop();
+        if(word==targetword) return steps;
+
+        for(int i=0;i<word.size();i++){
+            char original=word[i];
+            for(char ch='a'; ch<='z';ch++){
+                word[i]=ch;
+                if(st.find(word)!=st.end()){
+                    st.erase(word);
+                    q.push({word,steps+1});
+                }
+            }
+            word[i]=original;
+        }
+
+    }
+    return 0;
+}
--- c_22_graph/hard_prb/explain.md
+++ c_22_graph/hard_prb/explain.md
@@ -200,3 +200,29 @@ if take lowest
 ![alt text](image-69.png)
 
 ![alt text](image-70.png)
+
+word_ladder_1
+
+![alt text](image-71.png)
+
+![alt text](image-72.png)
+
+![alt text](image-73.png)
+
+![alt text](image-74.png)
+
+![alt text](image-75.png)
+
+![alt text](image-76.png)
+
+![alt text](image-77.png)
+
+once taken not need to take again na
+
+![alt text](image-78.png)
+
+![alt text](image-79.png)
+
+![alt text](image-80.png)
+
+![alt text](image-81.png)
